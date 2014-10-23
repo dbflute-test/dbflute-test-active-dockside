0c632bc3f55bfb02f2a99d5a9b57816b45a02649
jflute <dbflute@gmail.com>
2014-10-23 15:19:56 +0900

    pilot7: test OK

--- src/test/java/org/docksidestage/dockside/dbflute/whitebox/cbean/bigartist/derivedreferrer/WxCBDerivedReferrerDerivedMapTest.java
+++ src/test/java/org/docksidestage/dockside/dbflute/whitebox/cbean/bigartist/derivedreferrer/WxCBDerivedReferrerDerivedMapTest.java
@@ -1,6 +1,7 @@
 package org.docksidestage.dockside.dbflute.whitebox.cbean.bigartist.derivedreferrer;
 
 import org.docksidestage.dockside.dbflute.exbhv.MemberBhv;
+import org.docksidestage.dockside.dbflute.exentity.Member;
 import org.docksidestage.dockside.unit.UnitContainerTestCase;
 
 /**
@@ -12,8 +13,34 @@ public class WxCBDerivedReferrerDerivedMapTest extends UnitContainerTestCase {
     // ===================================================================================
     //                                                                           Attribute
     //                                                                           =========
-    @SuppressWarnings("unused")
     private MemberBhv memberBhv;
 
     // TODO jflute test: derived map
+
+    public void test_DerivedMap_basic() throws Exception {
+        // ## Arrange ##
+        String aliasName = "$maxPrice";
+
+        // ## Act ##
+        Member member = memberBhv.selectEntity(cb -> {
+            cb.specify().derivedPurchase().max(purchaseCB -> {
+                purchaseCB.specify().columnPurchasePrice();
+            }, aliasName);
+            cb.query().existsPurchase(purchaseCB -> {});
+            cb.fetchFirst(1);
+        });
+
+        // ## Assert ##
+        memberBhv.loadPurchase(member, purchaseCB -> {
+            purchaseCB.specify().columnPurchasePrice();
+            purchaseCB.query().addOrderBy_PurchaseDatetime_Asc();
+        });
+        member.derived(aliasName, Integer.class).alwaysPresent(price -> {
+            log(price);
+            Integer expected = member.getPurchaseList().stream().map(purchase -> {
+                return purchase.getPurchasePrice();
+            }).max((o1, o2) -> o1.compareTo(o2)).get();
+            assertEquals(expected, price);
+        });
+    }
 }
