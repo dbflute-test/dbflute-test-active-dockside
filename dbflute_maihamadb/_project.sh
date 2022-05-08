50e29df8f146d9bcf480e02d7220fb6bb59a9e18
jflute <jflute@localhost>
2022-05-09 01:09:02 +0900

    fixed java version for #java17 test

--- dbflute_maihamadb/_project.sh
+++ dbflute_maihamadb/_project.sh
@@ -7,6 +7,6 @@ export DBFLUTE_HOME=../mydbflute/dbflute-1.x
 export MY_PROPERTIES_PATH=build.properties
 
 if [[ `uname` = "Darwin" ]]; then
-  # fixed java version for #java11 test
-  export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+  # fixed java version for #java17 test
+  export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 fi
