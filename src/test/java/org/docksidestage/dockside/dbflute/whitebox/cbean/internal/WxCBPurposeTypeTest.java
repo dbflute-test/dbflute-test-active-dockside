c3460cfbfe3e8ad462016dd05712f8464886390a
jflute <dbflute@gmail.com>
2023-07-19 19:58:31 +0900

    adjust purpose type test

--- src/test/java/org/docksidestage/dockside/dbflute/whitebox/cbean/internal/WxCBPurposeTypeTest.java
+++ src/test/java/org/docksidestage/dockside/dbflute/whitebox/cbean/internal/WxCBPurposeTypeTest.java
@@ -3,6 +3,7 @@ package org.docksidestage.dockside.dbflute.whitebox.cbean.internal;
 import org.dbflute.cbean.scoping.SubQuery;
 import org.dbflute.exception.OrderByIllegalPurposeException;
 import org.dbflute.exception.SetupSelectIllegalPurposeException;
+import org.dbflute.exception.SpecifyIllegalPurposeException;
 import org.docksidestage.dockside.dbflute.cbean.MemberCB;
 import org.docksidestage.dockside.dbflute.cbean.PurchaseCB;
 import org.docksidestage.dockside.unit.UnitContainerTestCase;
@@ -13,12 +14,16 @@ import org.docksidestage.dockside.unit.UnitContainerTestCase;
  */
 public class WxCBPurposeTypeTest extends UnitContainerTestCase {
 
-    public void test_illegalPurpose_setupSelect() throws Exception {
+    // ===================================================================================
+    //                                                                      ExistsReferrer
+    //                                                                      ==============
+    public void test_illegalPurpose_ExistsReferrer_setupSelect() throws Exception {
         // ## Arrange ##
         MemberCB cb = new MemberCB();
         try {
             cb.query().existsPurchase(new SubQuery<PurchaseCB>() {
                 public void query(PurchaseCB subCB) {
+                    // ## Act ##
                     subCB.setupSelect_Member();
                 }
             });
@@ -29,14 +34,63 @@ public class WxCBPurposeTypeTest extends UnitContainerTestCase {
         }
     }
 
-    public void test_illegalPurpose_orderBy() throws Exception {
+    public void test_illegalPurpose_ExistsReferrer_orderBy() throws Exception {
         // ## Arrange ##
         MemberCB cb = new MemberCB();
         try {
             cb.query().existsPurchase(new SubQuery<PurchaseCB>() {
                 public void query(PurchaseCB subCB) {
+                    // ## Act ##
                     subCB.query().addOrderBy_MemberId_Asc();
                 }
+            });
+            // ## Assert ##
+            fail();
+        } catch (OrderByIllegalPurposeException e) {
+            log(e.getMessage());
+        }
+    }
+
+    // ===================================================================================
+    //                                                                        OrScopeQuery
+    //                                                                        ============
+    public void test_illegalPurpose_OrScopeQuery_setupSelect() throws Exception {
+        // ## Arrange ##
+        MemberCB cb = new MemberCB();
+        try {
+            cb.orScopeQuery(orCB -> {
+                // ## Act ##
+                orCB.setupSelect_MemberStatus();
+            });
+            // ## Assert ##
+            fail();
+        } catch (SetupSelectIllegalPurposeException e) {
+            log(e.getMessage());
+        }
+    }
+
+    public void test_illegalPurpose_OrScopeQuery_specify() throws Exception {
+        // ## Arrange ##
+        MemberCB cb = new MemberCB();
+        try {
+            cb.orScopeQuery(orCB -> {
+                // ## Act ##
+                orCB.specify();
+            });
+            // ## Assert ##
+            fail();
+        } catch (SpecifyIllegalPurposeException e) {
+            log(e.getMessage());
+        }
+    }
+
+    public void test_illegalPurpose_OrScopeQuery_orderBy() throws Exception {
+        // ## Arrange ##
+        MemberCB cb = new MemberCB();
+        try {
+            cb.orScopeQuery(orCB -> {
+                // ## Act ##
+                orCB.query().addOrderBy_MemberId_Asc();
             });
             // ## Assert ##
             fail();
